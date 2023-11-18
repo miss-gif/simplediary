f53908dbe2a9fece450cec4a8589c854b6db7766
miss <ddc.akari@gmail.com>
2023-11-18 09:55:05 +0900

    데이터 수정하기 기능

--- src/DiaryList.jsx
+++ src/DiaryList.jsx
@@ -1,13 +1,18 @@
 import DiaryItem from "./DiaryItem";
 
-const DiaryList = ({ onDelete, diaryList }) => {
+const DiaryList = ({ onEdit, onRemove, diaryList }) => {
   return (
     <div className="DiaryList">
       <h2>DiaryList</h2>
       <p>{diaryList.length}개의 일기가 있습니다.</p>
       <div>
         {diaryList.map((item) => (
-          <DiaryItem key={item.id} {...item} onDelete={onDelete} />
+          <DiaryItem
+            key={item.id}
+            {...item}
+            onEdit={onEdit}
+            onRemove={onRemove}
+          />
         ))}
       </div>
     </div>
